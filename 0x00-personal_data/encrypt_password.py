dd6bb0db1ed76ae1f082c900dbfd84e365aa3ebb
rosyrose1 <rosinaantwi96@gmail.com>
2024-03-13 14:21:04 -0700

    personal data

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python3
-""" Encrypting pswds with bcrypt """
+"""
+Defines a hash_password function to return a hashed password
+"""
 import bcrypt
+from bcrypt import hashpw
 
 
 def hash_password(password: str) -> bytes:
-    """ Takes in string arg, converts to unicode
-    Returns salted, hashed pswd as bytestring
     """
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    Returns a hashed password
+    Args:
+        password (str): password to be hashed
+    """
+    b = password.encode()
+    hashed = hashpw(b, bcrypt.gensalt())
+    return hashed
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """ Checks if hashed and unhashed pswds are same
-    Returns bool
     """
-    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+    Check whether a password is valid
+    Args:
+        hashed_password (bytes): hashed password
+        password (str): password in string
+    Return:
+        bool
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
